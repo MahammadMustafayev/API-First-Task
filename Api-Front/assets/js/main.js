41b9692a97c2e9552c51643fc322d95f78422192
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-06-21 19:16:21 +0300

    added

--- Api-Front/assets/js/main.js
+++ Api-Front/assets/js/main.js
@@ -1,45 +1,23 @@
 let form = document.getElementById("form");
 let img = document.getElementById("image");
 let url = "https://localhost:5001/api/actor";
-let row = document.querySelector(".row");
+let tr = document.getElementById("tr");
 
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
 
-    const data = {
-        FullName: $('#name').val(),
-        ImageUrl: $('#image').val()
-    }
-    $.ajax({
-        url: `${url}create`,
-        type: 'POST',
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (d) {
-            $('#name').val('');
-            $('#image').val('');
-        },
-        error: function () {
-            console.log("Error var");
-        }
-    });
-});
 
 fetch(url)
     .then(res => res.json())
     .then(data => {
         data.forEach(el => {
-            row.innerHTML = `
-            <div class="col-md-4">
-            <div style="width: 250px;">
-                <img src="${el.imageUrl}" width="150" height="200" class="card-img" alt="">
-                <div class="card-body">
-                    <h3 class="text-uppercase text-primary">${el.fullName}</h3>
-                </div>
-            </div>
-        </div>
+            tr.innerHTML = `
+            <td class="pt-5 fw-bold">${el.FullName}</td>
+                    <td><img id="image" src="${el.ImageUrl}"
+                            style="max-width:100px" draggable="false" /></td>
+                    <td class="pt-5">
+                        <a href="edit.html" class="btn btn-warning">Edit</a>
+                        <a href="#" class="btn btn-danger btn-sweet-delete">Delete</a>
+                    </td>
             `
         });
     })
